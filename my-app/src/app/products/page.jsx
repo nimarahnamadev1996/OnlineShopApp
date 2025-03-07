1c171873a5a0aec56c56baf146e3205880f05c28
nimarahnamadev1996 <nimarahnamadev1996@gmail.com>
2025-03-07 12:28:20 +0330

    adding

---
+++ my-app/src/app/products/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LatestProducts from '../../../components/LatestProducts'
+
+const Products = () => {
+  return (
+    <div>
+        <LatestProducts/>
+    </div>
+  )
+}
+
+export default Products
